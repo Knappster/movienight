6672740ede07da34b6b4722580f660e0c2fb4a87
Carl Knapp <kn4ppster@gmail.com>
2019-08-16 17:57:39 +0100

    Optimized colors list

--- movienight-client/src/components/UserName.vue
+++ movienight-client/src/components/UserName.vue
@@ -9,65 +9,21 @@
 				<h3 class="username__label">Pick a colour:</h3>
 
 				<div class="username__colors">
-					<div class="username__colors__color">
-						<input id="username-color-ff0000" type="radio" value="ff0000" v-model="user.color">
-						<label for="username-color-ff0000" class="username__colors__label username__colors__label--ff0000"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-0000ff" type="radio" value="0000ff" v-model="user.color">
-						<label for="username-color-0000ff" class="username__colors__label username__colors__label--0000ff"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-008000" type="radio" value="008000" v-model="user.color">
-						<label for="username-color-008000" class="username__colors__label username__colors__label--008000"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-b22222" type="radio" value="b22222" v-model="user.color">
-						<label for="username-color-b22222" class="username__colors__label username__colors__label--b22222"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-ff7f50" type="radio" value="ff7f50" v-model="user.color">
-						<label for="username-color-ff7f50" class="username__colors__label username__colors__label--ff7f50"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-9acd32" type="radio" value="9acd32" v-model="user.color">
-						<label for="username-color-9acd32" class="username__colors__label username__colors__label--9acd32"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-ff4500" type="radio" value="ff4500" v-model="user.color">
-						<label for="username-color-ff4500" class="username__colors__label username__colors__label--ff4500"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-2e8b57" type="radio" value="2e8b57" v-model="user.color">
-						<label for="username-color-2e8b57" class="username__colors__label username__colors__label--2e8b57"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-daa520" type="radio" value="daa520" v-model="user.color">
-						<label for="username-color-daa520" class="username__colors__label username__colors__label--daa520"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-d2691e" type="radio" value="d2691e" v-model="user.color">
-						<label for="username-color-d2691e" class="username__colors__label username__colors__label--d2691e"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-5f9ea0" type="radio" value="5f9ea0" v-model="user.color">
-						<label for="username-color-5f9ea0" class="username__colors__label username__colors__label--5f9ea0"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-1e90ff" type="radio" value="1e90ff" v-model="user.color">
-						<label for="username-color-1e90ff" class="username__colors__label username__colors__label--1e90ff"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-ff69b4" type="radio" value="ff69b4" v-model="user.color">
-						<label for="username-color-ff69b4" class="username__colors__label username__colors__label--ff69b4"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-8a2be2" type="radio" value="8a2be2" v-model="user.color">
-						<label for="username-color-8a2be2" class="username__colors__label username__colors__label--8a2be2"></label>
-					</div>
-					<div class="username__colors__color">
-						<input id="username-color-00ff7f" type="radio" value="00ff7f" v-model="user.color">
-						<label for="username-color-00ff7f" class="username__colors__label username__colors__label--00ff7f"></label>
+					<div class="username__colors__color"
+						v-for="color in colors"
+						:key="color.id"
+					>
+						<input
+							:id="'username-color-'+color"
+							type="radio"
+							:value="color"
+							v-model="user.color"
+						>
+						<label
+							:for="'username-color-'+color"
+							class="username__colors__label"
+							:style="{backgroundColor: '#'+color}">
+						</label>
 					</div>
 				</div>
 			</div>
@@ -88,6 +44,24 @@ export default {
 				, color: 'ffffff'
 			}
 			, error: false
+			, colors: [
+				'ff0000'
+				, '0000ff'
+				, '008000'
+				, 'b22222'
+				, 'ff7f50'
+				, '9acd32'
+				, 'ff4500'
+				, '2e8b57'
+				, 'daa520'
+				, 'd2691e'
+				, '5f9ea0'
+				, '1e90ff'
+				, 'ff69b4'
+				, '8a2be2'
+				, '00ff7f'
+				, 'ff00ce'
+			]
 		}
 	}
 	, mounted() {
@@ -184,66 +158,6 @@ export default {
 				display: block;
 				width: 25px;
 				height: 25px;
-				
-				&--ff0000 {
-					background-color: #ff0000;
-				}
-
-				&--0000ff {
-					background-color: #0000ff;
-				}
-
-				&--008000 {
-					background-color: #008000;
-				}
-
-				&--b22222 {
-					background-color: #b22222;
-				}
-
-				&--ff7f50 {
-					background-color: #ff7f50;
-				}
-
-				&--9acd32 {
-					background-color: #9acd32;
-				}
-
-				&--ff4500 {
-					background-color: #ff4500;
-				}
-
-				&--2e8b57 {
-					background-color: #2e8b57;
-				}
-
-				&--daa520 {
-					background-color: #daa520;
-				}
-
-				&--d2691e {
-					background-color: #d2691e;
-				}
-
-				&--5f9ea0 {
-					background-color: #5f9ea0;
-				}
-
-				&--1e90ff {
-					background-color: #1e90ff;
-				}
-				
-				&--ff69b4 {
-					background-color: #ff69b4;
-				}
-
-				&--8a2be2 {
-					background-color: #8a2be2;
-				}
-
-				&--00ff7f {
-					background-color: #00ff7f;
-				}
 			}
 		}
 	}
